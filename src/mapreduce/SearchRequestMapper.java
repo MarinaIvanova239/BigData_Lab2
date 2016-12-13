1d79441070d8addaf00fd28ce10d390a56de98d2
MarinaIvanova239 <mar.ivanova@bk.ru>
2016-12-13 21:36:43 +0300

    add stemmer to pom file and mapper

--- src/mapreduce/SearchRequestMapper.java
+++ src/mapreduce/SearchRequestMapper.java
@@ -8,20 +8,27 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
+import org.tartarus.snowball.SnowballStemmer;
+import org.tartarus.snowball.ext.porterStemmer;
+
 public class SearchRequestMapper extends Mapper<LongWritable, Text, Text, Text>  {
 
-    private Text word = new Text();
+    private Text stemWord = new Text();
 
     @Override
     public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
 
+        SnowballStemmer stemmer = new porterStemmer();
         String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
         String line = value.toString();
         StringTokenizer tokenizer = new StringTokenizer(line);
         while (tokenizer.hasMoreTokens()) {
-            word.set(tokenizer.nextToken());
-            context.write(word, new Text(fileName));
+            String word = tokenizer.nextToken().toLowerCase();
+            stemmer.setCurrent(word);
+            stemmer.stem();
+            stemWord.set(stemmer.getCurrent());
+            context.write(stemWord, new Text(fileName));
         }
     }
 }
